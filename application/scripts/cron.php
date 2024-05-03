7157450f2dc03491e1eb1d7908a16dc7d643402c
jonathan-llemit-dev <jonathan.llemit.dev@gmail.com>
2024-05-03 13:09:06 +0800

    added cron.php

---
+++ application/scripts/cron.php
@@ -0,0 +1,10 @@
+<?php
+// Define the path to your CodeIgniter index.php file
+define('BASEPATH', __DIR__ . '/../');
+require_once BASEPATH . 'index.php';
+
+// Get an instance of the controller
+$CI =& get_instance();
+
+// Call the scheduledAddingOfData() method from your controller
+$CI->load->controller('students_portal/StudentsPortalController/scheduledAddingOfData');
